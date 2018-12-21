65c43eda38cce88a3a456839f11d0116e2883c54
Simon Drouin <simdrouin@gmail.com>
2018-12-21 15:12:34 -0500

    Added reference to lib-header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+    token: null
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'AUTH':
+            return {...state, token: '123456' };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
